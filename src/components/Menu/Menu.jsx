9e44dd9f66fdf75656984f41a1a7129d6695be8a
Prashant Bhardwaj <prashant.b@zengatv.com>
2023-04-11 16:35:15 +0530

    New Changes

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -14,54 +14,14 @@ const Menu = ({ innerPage }) => {
       <div className="container">
         <div className="products-container">
           <div className="products">
-            <Menuitems
-              id={Mdata[0].id}
-              imgsrc={Mdata[0].imgsrc}
-              pname={Mdata[0].pname}
-              price={Mdata[0].price}
-            />
-            <Menuitems
-              id={Mdata[1].id}
-              imgsrc={Mdata[1].imgsrc}
-              pname={Mdata[1].pname}
-              price={Mdata[1].price}
-            />
-            <Menuitems
-              id={Mdata[2].id}
-              imgsrc={Mdata[2].imgsrc}
-              pname={Mdata[2].pname}
-              price={Mdata[2].price}
-            />
-            <Menuitems
-              id={Mdata[3].id}
-              imgsrc={Mdata[3].imgsrc}
-              pname={Mdata[3].pname}
-              price={Mdata[3].price}
-            />
-            <Menuitems
-              id={Mdata[4].id}
-              imgsrc={Mdata[4].imgsrc}
-              pname={Mdata[4].pname}
-              price={Mdata[4].price}
-            />
-            <Menuitems
-              id={Mdata[5].id}
-              imgsrc={Mdata[5].imgsrc}
-              pname={Mdata[5].pname}
-              price={Mdata[5].price}
-            />
-            <Menuitems
-              id={Mdata[6].id}
-              imgsrc={Mdata[6].imgsrc}
-              pname={Mdata[6].pname}
-              price={Mdata[6].price}
-            />
-            <Menuitems
-              id={Mdata[7].id}
-              imgsrc={Mdata[7].imgsrc}
-              pname={Mdata[7].pname}
-              price={Mdata[7].price}
-            />
+            {Mdata.map((menu) => (
+              <Menuitems
+                id={menu.id}
+                imgsrc={menu.imgsrc}
+                pname={menu.pname}
+                price={menu.price}
+              />
+            ))}
           </div>
         </div>
       </div>
@@ -91,8 +51,8 @@ const Wrapper = styled.section`
       flex-flow: wrap;
       align-items: flex-start;
       justify-content: center;
-      column-gap: 2.5rem;
-      row-gap: 4.5rem;
+      row-gap: 3rem;
+      column-gap: 1.5rem;
     }
   }
   @media only screen and (min-width: ${({ theme }) => theme.media.tab}) {
@@ -100,7 +60,15 @@ const Wrapper = styled.section`
       font-size: 4rem;
     }
     .products-container {
-      margin: 5rem 0 12rem;
+      margin: 10rem 0;
+      .products {
+        display: flex;
+        flex-flow: wrap;
+        align-items: flex-start;
+        justify-content: center;
+        column-gap: 3rem;
+        row-gap: 4.5rem;
+      }
     }
   }
 `;
